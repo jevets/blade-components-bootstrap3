0f10dcf26ff460c029ab0cdf87b62c96b0ea692e
stevejamesson <steve@evendev.com>
2017-08-02 23:04:05 -0400

    initial commit

---
+++ resources/views/$/button.blade.php
@@ -0,0 +1,13 @@
+<a href="{{ $url or '#' }}" class="btn btn-{{ $type or 'default' }} btn-{{ $size or 'sm' }} {{ $classes or '' }}">
+    @isset ($content)
+        {{ $content }}
+    @else
+        @if (isset($icon) || isset($icon_left))
+            <i class="fa fa-{{ $icon or $icon_left }}"></i>
+        @endif
+        {{ $text or '' }}
+        @isset ($icon_right)
+            <i class="fa fa-{{ $icon_right }}"></i>
+        @endisset
+    @endif
+</a>
